b1ab8900596c8defbbad2a0f530dd537b81e867b
Brandon Aubie <brandon@aubie.ca>
2010-07-13 14:25:33 -0400

    New data

--- runsims.py
+++ runsims.py
@@ -2,11 +2,11 @@
 import dtn
 
 
-dtn.runSimulation("",[], []) 
-quit()
+#dtn.runSimulation("",[], []) 
+#quit()
 
-for i in range(0, 10):
-    inh = i*0.0005+0.003
+for i in range(0, 3):
+    inh = i*0.0010+0.003
     name = "EXCRamp2/EXCRamp-%.4f"%inh
     dtn.runSimulation(name, [["Exc", inh]], [])
 
